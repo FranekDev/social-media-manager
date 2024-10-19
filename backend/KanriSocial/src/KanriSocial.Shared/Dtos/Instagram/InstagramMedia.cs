8d1e38cfb95521f4ae0ef001a5c18d181ff20f53
Franek Wojtas <49079734+FranekDev@users.noreply.github.com>
2024-10-19 11:15:14 +0200

    feat(backend): add shared dtos

---
+++ backend/KanriSocial/src/KanriSocial.Shared/Dtos/Instagram/InstagramMedia.cs
@@ -0,0 +1,3 @@
+﻿namespace KanriSocial.Shared.Dtos.Instagram;
+
+public record InstagramMedia(string Id);
---
+++ backend/KanriSocial/src/KanriSocial.Shared/Dtos/Instagram/InstagramUserDetail.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace KanriSocial.Shared.Dtos.Instagram;
+
+public record InstagramUserDetail
+{
+    public string UserName { get; init; }
+
+    public string Biography { get; init; }
+
+    [JsonProperty("media_count")]
+    public int MediaCount { get; init; }
+
+    [JsonProperty("followers_count")]
+    public int FollowersCount { get; init; }
+
+    [JsonProperty("follows_count")]
+    public int FollowsCount { get; init; }
+
+    public string Name { get; init; }
+
+    [JsonProperty("profile_picture_url")]
+    public Uri ProfilePictureUrl { get; init; }
+
+    public Uri Website { get; init; }
+}
